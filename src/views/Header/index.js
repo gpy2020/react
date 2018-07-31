c96d2380d971ea53a76ab19ea6aa206c663df256
VistriblE <Mikhail.Anishchanka@itechart-group.com>
2018-07-31 10:42:22 +0300

    fixed task3

--- src/views/Header/index.js
+++ src/views/Header/index.js
@@ -3,18 +3,19 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { Link, withRouter } from 'react-router-dom';
+import { styles } from './style';
+import { withStyles } from '@material-ui/core/styles'
 
 
 function header(props) {
+    const {classes} = props;
     return (
-        <div>
-            <AppBar position="static">
+            <AppBar className={classes.headerAppBar}>
             <Tabs value={props.value} onChange={props.handleChange} centered>
                 <Tab component={Link} to='/about' label='О нас'/>
                 <Tab component={Link} to='/counters' label='Счетчики'/>
             </Tabs>
             </AppBar>
-        </div>
     )
 }
-export default withRouter(header);
+export default withStyles(styles)(withRouter(header));
